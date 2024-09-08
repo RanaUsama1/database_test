0b849818460d460759b1126fa5db3b847319a8a1
MDAbdullah <ranausama02@gmail.com>
2024-09-08 17:27:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
   const ret = {
+    base: '/database_test/',
     plugins: [vue()],
     resolve: {
       alias: {
