a5c96d8591dc1b5d342854cee09ef9dc39006097
Muhammad Abdullah <ranausama02@gmail.com>
2024-09-11 13:57:36 +0200

    Your descriptive commit message

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,33 @@
-import { fileURLToPath, URL } from "node:url";
+// import { fileURLToPath, URL } from "node:url";
+// import { defineConfig } from "vite";
+// import vue from "@vitejs/plugin-vue";
 
+// // https://vitejs.dev/config/
+// export default defineConfig(({ command, mode, ssrBuild }) => {
+//   const ret = {
+//     base: "/database_test/",
+//     plugins: [vue()],
+//     resolve: {
+//       alias: {
+//         "@": fileURLToPath(new URL("./src", import.meta.url)),
+//       },
+//     },
+//   };
+//   ret.define = {
+//     __API_URL__: JSON.stringify("http://localhost:3000"),
+//   };
+//   return ret;
+// });
+
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
+  const isProduction = mode === "production";
   const ret = {
-    base: "/database_test/",
-
+    base: isProduction ? "/database_test/" : "/",
     plugins: [vue()],
     resolve: {
       alias: {
@@ -15,8 +35,14 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
       },
     },
   };
+
   ret.define = {
-    __API_URL__: JSON.stringify("http://localhost:3000"),
+    __API_URL__: JSON.stringify(
+      isProduction
+        ? "https://ranausama1.github.io/database_test/"
+        : "http://localhost:3000"
+    ),
   };
+
   return ret;
 });
