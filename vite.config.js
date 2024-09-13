191d5db4a4ecd72dfe4299dcd8e6749b92667b0f
Muhammad Abdullah <ranausama02@gmail.com>
2024-09-13 16:52:21 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -19,9 +19,9 @@
 //   return ret;
 // });
 
-import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+// import { fileURLToPath, URL } from "node:url";
+// import { defineConfig } from "vite";
+// import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 // export default defineConfig(({ command, mode, ssrBuild }) => {
@@ -60,8 +60,26 @@ import vue from "@vitejs/plugin-vue";
 //   };
 // });
 
-export default defineConfig({
-  // base: process.env.NODE_ENV === "production" ? "/database_test/" : "/",
-  base: "/database_test/",
-  plugins: [vue()],
+// export default defineConfig({
+//   // base: process.env.NODE_ENV === "production" ? "/database_test/" : "/",
+//   base: "/database_test/",
+//   plugins: [vue()],
+// });
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
+
+export default defineConfig(({ command, mode }) => {
+  const isProduction = mode === "production";
+
+  return {
+    base: isProduction ? "/database_test/" : "/", // Set base path for GitHub Pages
+    plugins: [vue()], // Use Vue plugin
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)), // Alias for src
+      },
+    },
+  };
 });
